14debc73627ac256ee3a3ebd9e170d5fb1a1840d
mnhthompson <mnhthompson@gmail.com>
2025-06-03 19:51:01 -0700

    fix 2

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,15 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         quiz: resolve(__dirname, "src/quiz.html"),
+        quiz1: resolve(__dirname, "src/quizkanto.html"),
+        quiz2: resolve(__dirname, "src/quizjohto.html"),
+        quiz3: resolve(__dirname, "src/quizhoenn.html"),
+        quiz4: resolve(__dirname, "src/quizsinnoh.html"),
+        quiz5: resolve(__dirname, "src/quizunova.html"),
+        quiz6: resolve(__dirname, "src/quizkalos.html"),
+        quiz7: resolve(__dirname, "src/quizalola.html"),
+        quiz8: resolve(__dirname, "src/quizgalar.html"),
+        quiz9: resolve(__dirname, "src/quizpaldea.html"),
         results: resolve(__dirname, "src/results.html")
       }
     }
